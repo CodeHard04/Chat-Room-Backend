a3d931187140cc85da90632415039cb8fd221090
Atishay Jain(atishay.jain02) <atishay.jain02@infosys.com>
2023-05-14 14:16:26 +0530

    Manaals code updates

--- ChatRoom/server.js
+++ ChatRoom/server.js
@@ -13,8 +13,18 @@ const authentication = require("./Middlewares/authentication");
 const authController = require("./Controllers/authController");
 const CustomError = require("./Utilities/customError");
 const chatRouter = require("./Routes/chatRoute");
+const jwt = require("jsonwebtoken");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
+const server = http.createServer(app);
 app.use(cors());
+
+const { InMemorySessionStore } = require("./sessionStore");
+const sessionStore = new InMemorySessionStore();
+
+const { InMemoryMessageStore } = require("./messageStore");
+const messageStore = new InMemoryMessageStore();
 
 app.use(helmet());
 dotenv.config();
@@ -26,6 +36,13 @@ const limiter = rateLimit({
   message: "Too many rquest from thi ip please try again in a hour",
 });
 app.use("/", limiter);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
 app.use(express.json());
 
 app.post("/signup", authController.saveUserData);
@@ -58,6 +75,111 @@ app.listen(5000, () => {
   //   }).catch((error) => {
   //     console.error('Unable to create table : ', error);
   //   });
+});
+
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    // find existing session
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      socket.userID = session.userID;
+      socket.username = session.name;
+      return next();
+    }
+  }
+  const token = socket.handshake.auth.sessionID;
+  const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  console.log(data, "%%%%%%%% data");
+  if (!data) {
+    return next(new Error("invalid username"));
+  }
+
+  // To create new session
+  socket.sessionID = token;
+  socket.userID = data.userId;
+  socket.username = data.name;
+  next();
+});
+
+io.on("connection", (socket) => {
+  // persist session
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true,
+  });
+
+  // emit session details
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+
+  // join the "userID" room
+  socket.join(socket.userID);
+
+  console.log("connected!!", socket.userID);
+  // fetch existing users
+  const users = [];
+  const messagesPerUser = new Map();
+  messageStore.findMessagesForUser(socket.userID).forEach((message) => {
+    const { from, to } = message;
+    const otherUser = socket.userID === from ? to : from;
+    if (messagesPerUser.has(otherUser)) {
+      messagesPerUser.get(otherUser).push(message);
+    } else {
+      messagesPerUser.set(otherUser, [message]);
+    }
+  });
+  sessionStore.findAllSessions().forEach((session) => {
+    users.push({
+      userID: session.userID,
+      username: session.username,
+      connected: session.connected,
+      messages: messagesPerUser.get(session.userID) || [],
+    });
+  });
+  socket.emit("users", users);
+
+  // notify existing users
+  socket.broadcast.emit("user connected", {
+    userID: socket.userID,
+    username: socket.username,
+    users,
+  });
+
+  // forward the private message to the right recipient (and to other tabs of the sender)
+  socket.on("private-message", ({ updateMsg, to }) => {
+    const message = {
+      updateMsg,
+      from: socket.userID,
+      to,
+    };
+    socket.to(to).to(socket.userID).emit("private-message", message);
+    messageStore.saveMessage(message);
+  });
+
+  socket.on("connect_error", (data) => {
+    console.log(data, "888888888888 connect_error");
+  });
+
+  socket.on("disconnect", async () => {
+    const matchingSockets = await io.in(socket.userID).allSockets();
+    console.log(matchingSockets, "-----------matchingSockets");
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // notify other users
+      socket.broadcast.emit("user disconnected", socket.userID);
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false,
+      });
+    }
+  });
 });
 
 module.exports = app;
