2a080c755075ae7c4eb66b546ce3db3da88d7158
Atishay Jain(atishay.jain02) <atishay.jain02@infosys.com>
2023-05-20 04:52:16 +0530

    Updates the code for filter api

--- ChatRoom/Controllers/userController.js
+++ ChatRoom/Controllers/userController.js
@@ -3,10 +3,12 @@ const bcrypt = require("bcryptjs");
 const { Client } = require("@elastic/elasticsearch");
 const { User } = require("../Models/User");
 const catchAsyncError = require("../Utilities/catchAsyncError");
+const https = require("https");
 const CustomError = require("../Utilities/customError");
 const { dbSetup } = require("../Models/dbConnection");
 const elastic = require("../Utilities/elasticSearch");
 const axios = require("axios");
+const { Sequelize } = require("sequelize");
 
 class userController {
   getUserData = catchAsyncError(async (req, res, next) => {
@@ -41,26 +43,55 @@ class userController {
   });
   //Filter api
   //age gender country
-
   filterUser = catchAsyncError(async (req, res, next) => {
+    const agent = new https.Agent({ rejectUnauthorized: false });
     const page = parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const startIdx = (page - 1) * limit;
-    let regionFilter = {
-      India: ["India", "Pakistan", "Srilanka"],
-      America: ["America", "UK", "Malaysia"],
-      Japan: ["Japan", "China", "Korea"],
-      Russia: ["Russia", "Korea", "China"],
-      HongKong: ["HongoKong", "Switzerland", "Singapore"],
-    };
+    const { gender, age, country } = req.body;
 
     // Make request
-    axios
-      .get("https://restcountries.com/v3.1/all")
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
+    const countries = await axios.get(
+      "https://restcountries.com/v3.1/all?fields=name,latlng",
+      {
+        httpsAgent: agent,
+      }
+    );
+    let reference;
+    countries.data.map((referenceCountry) => {
+      if (referenceCountry.name.common === country) {
+        reference = referenceCountry;
+      }
+    });
+    function calculateDistance(lat1, lon1, lat2, lon2) {
+      const earthRadius = 6371;
+      const dlat = ((lat2 - lat1) * Math.PI) / 180;
+      const dlon = ((lon2 - lon1) * Math.PI) / 180;
+      const a =
+        Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+        Math.cos((lat1 * Math.PI) / 180) *
+          Math.cos((lat2 * Math.PI) / 180) *
+          Math.sin(dlon / 2) *
+          Math.sin(dlon / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distance = earthRadius * c;
+      return distance;
+    }
+    const distances = countries.data.map((country) => {
+      const distance = calculateDistance(
+        reference.latlng[0],
+        reference.latlng[1],
+        country.latlng[0],
+        country.latlng[1]
+      );
+      return {
+        name: country.name.common,
+        distance: distance,
+      };
+    });
+    distances.sort((a, b) => a.distance - b.distance);
+    const sortedCountries = distances.map((country) => country.name);
     let sequelize = dbSetup("chatDB");
-    const { gender, age, country } = req.body;
     sequelize
       .query(
         "SELECT * FROM Users WHERE gender=? AND age BETWEEN ? and ? AND country in (?) ORDER BY FIELD (country,?) LIMIT ?, ? ",
@@ -69,8 +100,8 @@ class userController {
             gender,
             age - 10,
             age + 10,
-            regionFilter[country],
-            regionFilter[country],
+            sortedCountries,
+            sortedCountries,
             startIdx,
             limit,
           ],
@@ -93,55 +124,31 @@ class userController {
           });
         }
       });
-    // const [results, metadata] =await sequelize.query(
-    //     'SELECT * FROM Users'
-    //     // {
-    //     //   replacements: ['active'],
-    //     //   type: QueryTypes.SELECT
-    //     // }
-    //   );
-    //   console.log(results);
-    //     User.findAll({
-    //         where: {
-    //             [Op.and]: [
-    //                 {"gender":gender},
-    //                 {"country":{
-    //                     [Op.in]:this.regionFilter[country]
-    //                     }
-    //                 },
-    //                 {"age":{
-    //                     [Op.between]:[age-10,age+10]
-    //                 }
-    //             }]
-    //         },
-    //         attributes: ['userId','name','country','gender','age']
-    //     }).then(result=>{
-    //         res.send(result);
-    //     })
   });
 
   getContact = catchAsyncError(async (req, res, next) => {
-    let sequelize = dbSetup("chatDB");
-    sequelize
-      .query("select contacts from Users where userId = ?", {
-        replacements: [req.query.userId],
-      })
-      .then((result) => {
-        let contactArray = result[0][0].contacts.split("#");
-        sequelize
-          .query("select name from Users where userId in (?)", {
-            replacements: [contactArray],
-          })
-          .then((result2) => {
-            res.send(result2[0]);
-          });
+    User.findAll({
+      attributes: ["contacts"],
+      where: { userId: req.query.userId },
+    }).then((result) => {
+      let contactArray = result[0].contacts.split("#");
+      User.findAll({
+        attributes: ["name"],
+        where: {
+          userId: {
+            [Sequelize.Op.in]: contactArray,
+          },
+        },
+      }).then((result2) => {
+        res.send(result2);
       });
+    });
   });
 
   searchUser = catchAsyncError(async (req, res, next) => {
-    console.log(elastic.addDocument("users", "atishay"));
-    let prefixData = await elastic.prefixSearch("users", "atish");
-    let fuzzyData = await elastic.fuzzySearch("users", "ati");
+    // console.log(elastic.addDocument("users", "atishay"));
+    let prefixData = await elastic.prefixSearch("users", req.query.value);
+    let fuzzyData = await elastic.fuzzySearch("users", req.query.value);
 
     return res.json({
       prefixResult: prefixData.hits.hits,
