6279f8b2c7739656938f838b686ec7aab562998d
Atishay Jain(atishay.jain02) <atishay.jain02@infosys.com>
2023-06-09 11:40:06 +0530

    cor error handling

--- ChatRoom/server.js
+++ ChatRoom/server.js
@@ -24,9 +24,17 @@ var corsOptions = {
   optionsSuccessStatus: true,
   credentials: true,
 };
-app.options("*", cors(corsOptions));
-// app.use(cors(corsOptions));
-
+// app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 const { InMemorySessionStore } = require("./sessionStore");
 const sessionStore = new InMemorySessionStore();
 
